76e7bd2ca6f5f0b01a9edbddb083b7be493ae3c5
Thingk0 <audtjd9080@naver.com>
2022-10-03 11:33:10 +0900

    CLUB_BOARD project :)

---
+++ src/main/java/org/club_board/ClubBoardApplication.java
@@ -0,0 +1,13 @@
+package org.club_board;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClubBoardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClubBoardApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/club_board/ClubBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package org.club_board;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClubBoardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
