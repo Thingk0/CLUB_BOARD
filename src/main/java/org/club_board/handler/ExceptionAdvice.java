aebed8930a9cfeaefe6313f9e9f95ffa4508785f
Thingk0 <audtjd9080@naver.com>
2022-10-04 16:30:47 +0900

    Feat: 	handler ( ExceptionAdvice ) Refactor: 	h2DB -> MariaDB 데이터베이스 변경

---
+++ src/main/java/org/club_board/handler/ExceptionAdvice.java
@@ -0,0 +1,23 @@
+package org.club_board.handler;
+
+import org.club_board.response.Response;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ExceptionAdvice {
+
+    /**
+     *
+     ExceptionHandler 어노테이션 괄호 안에 IllegalArgumentException.class 를 통해,
+     IllegalArgumentException 으로 오류를 보내주면 해당 메소드를 타게 된다..
+     */
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public Response<?> illegalArgumentExceptionAdvice(IllegalArgumentException e) {
+        return new Response<>("fail", e.getMessage().toString(), null);
+    }
+}
